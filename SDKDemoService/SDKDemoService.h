3fe9d8e9d71a8f0378476d089dc0f209ece865d9
harielakanti <harielakanti@gmail.com>
2019-12-16 15:29:56 +0530

    Initial Commit

---
+++ SDKDemoService/SDKDemoService.h
@@ -0,0 +1,19 @@
+//
+//  SDKDemoService.h
+//  SDKDemoService
+//
+//  Created by Hari_Vrinda on 12/16/19.
+//  Copyright © 2019 Hari_Vrinda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKDemoService.
+FOUNDATION_EXPORT double SDKDemoServiceVersionNumber;
+
+//! Project version string for SDKDemoService.
+FOUNDATION_EXPORT const unsigned char SDKDemoServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKDemoService/PublicHeader.h>
+
+
