6e0fe7c5ac11e30cd075b6a85cd8ca65ba2a0c60
harielakanti <harielakanti@gmail.com>
2019-12-16 15:42:09 +0530

    Initial Project SetUp

---
+++ SDKDemoServiceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDKDemoServiceExample
+//
+//  Created by Hari_Vrinda on 12/16/19.
+//  Copyright © 2019 Hari_Vrinda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
